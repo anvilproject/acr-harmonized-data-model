5b0d920b562fe5f0dddd09dfc16c41bfa74f7857
Robert Carroll <Robert.Carroll@vumc.org>
2026-01-28 12:55:07 -0600

    Update Copier Version

---
+++ src/acr_harmonized_data_model/__init__.py
@@ -0,0 +1,5 @@
+try:
+    from acr_harmonized_data_model._version import __version__, __version_tuple__
+except ImportError:  # pragma: no cover
+    __version__ = "0.0.0"
+    __version_tuple__ = (0, 0, 0)
